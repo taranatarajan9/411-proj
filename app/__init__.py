8f796c46e4fbeca6776b00eba185c5fa3bdc58e6
taranatarajan9 <52177429+taranatarajan9@users.noreply.github.com>
2021-11-24 21:48:15 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,15 @@
-import os
-import sqlalchemy
-from yaml import load, Loader
-
 from flask import Flask
-from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
-def init_connect_engine():
-    if os.environ.get('GAE_ENV') != 'standard':
-        variables = load(open("app.yaml"), Loader=Loader)
-        env_variables = variables['env_variables']
-        for var in env_variables:
-            os.environ[var] = env_variables[var]
-
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL(
-            drivername="mysql+pymysql",
-            username=os.environ.get('MYSQL_USER'),
-            password=os.environ.get('MYSQl_PASSWORD'),
-            database=os.environ.get('MYSQL_DB'),
-            host=os.environ.get('MYSQL_HOST')
-        )
-    )
-    return pool
-
-db = init_connect_engine()
-
-conn = db.connect()
-results = conn.execute("Select * from tasks;").fetchall()
-print(x for x in results)
-conn.close()
-
-@app.route("/")
-def homepage():
-    return jsonify({"status": "OK"})
-    return render_template("index.html", name = "Tara")
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import routes
